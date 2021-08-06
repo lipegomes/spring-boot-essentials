6f5c794e93c9cf87cdb66bf06552b3a82557e842
filipe gomes <fgdl.py91@gmail.com>
2021-08-05 21:53:59 -0300

    Adicionado start, domain, controller

--- src/main/java/dev/filipegomes/springboot2/controller/CountryController.java
+++ src/main/java/dev/filipegomes/springboot2/controller/CountryController.java
@@ -1,2 +1,17 @@
-package dev.filipegomes.springboot2.controller;public class CountryController {
+package dev.filipegomes.springboot2.controller;
+
+import dev.filipegomes.springboot2.domain.Country;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("country")
+public class CountryController {
+    @GetMapping(path = "list")
+    public List<Country> list(){
+        return List.of(new Country("Brazil"), new Country("Nigeria") , new Country("India"));
+    }
 }
--- src/main/java/dev/filipegomes/springboot2/start/ApplicationStart.java
+++ src/main/java/dev/filipegomes/springboot2/start/ApplicationStart.java
@@ -1,4 +1,13 @@
 package dev.filipegomes.springboot2.start;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+
+@EnableAutoConfiguration
+@ComponentScan(basePackages = "dev.filipegomes.springboot2")
 public class ApplicationStart {
+    public static void main (String[] args) {
+        SpringApplication.run(ApplicationStart.class, args);
+    }
 }
