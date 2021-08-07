016ced4488ae9c5fa48ab151910630ac7d65be6e
filipe gomes <fgdl.py91@gmail.com>
2021-08-07 15:05:35 -0300

    08 - Método GET parte 2

--- src/main/java/dev/filipegomes/springboot2/controller/CountryController.java
+++ src/main/java/dev/filipegomes/springboot2/controller/CountryController.java
@@ -5,7 +5,10 @@ import dev.filipegomes.springboot2.service.CountryService;
 import dev.filipegomes.springboot2.util.DateUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,7 +16,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
-@RequestMapping("country")
+@RequestMapping("countries")
 @Log4j2
 @RequiredArgsConstructor
 public class CountryController {
@@ -21,8 +24,13 @@ public class CountryController {
     private final CountryService countryService;
 
     @GetMapping
-    public List<Country> list() {
+    public ResponseEntity<List<Country>> list() {
         log.info(dateUtil.formatLocalDateTimeToDataBasStyle(LocalDateTime.now()));
-        return countryService.listAll();
+        return new ResponseEntity<>(countryService.listAll(), HttpStatus.OK);
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Country> findById(@PathVariable Integer id) {
+        return ResponseEntity.ok(countryService.findById(id));
     }
 }
--- src/main/java/dev/filipegomes/springboot2/service/CountryService.java
+++ src/main/java/dev/filipegomes/springboot2/service/CountryService.java
@@ -1,13 +1,25 @@
 package dev.filipegomes.springboot2.service;
 
 import dev.filipegomes.springboot2.domain.Country;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
 @Service
 public class CountryService {
+    private List<Country> countries = List.of(new Country(6, "Brazil"), new Country(94, "Tajikistan"), new Country(2, "India"));
+
     public List<Country> listAll() {
-        return List.of(new Country(6, "Brazil"), new Country(94, "Tajikistan"), new Country(2, "India"));
+        return countries;
+    }
+
+    public Country findById(Integer id) {
+        return countries
+                .stream()
+                .filter(country -> country.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "Country not Found"));
     }
 }
