fe2e5986523282ac4b408180d47a922b53c20fe1
filipe gomes <fgdl.py91@gmail.com>
2021-08-09 12:56:22 -0300

    13 - Spring Data JPA pt 01

--- src/main/java/dev/filipegomes/springboot2/domain/Product.java
+++ src/main/java/dev/filipegomes/springboot2/domain/Product.java
@@ -2,10 +2,20 @@ package dev.filipegomes.springboot2.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
 }
