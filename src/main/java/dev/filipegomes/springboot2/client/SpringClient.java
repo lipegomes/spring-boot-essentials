7dd7e7939d0452c010c328c235e87c5c8c44f7c5
filipe gomes <fgdl.py91@gmail.com>
2021-08-10 15:41:28 -0300

    26 - RestTemplate getForObject e getForEntity

---
+++ src/main/java/dev/filipegomes/springboot2/client/SpringClient.java
@@ -0,0 +1,18 @@
+package dev.filipegomes.springboot2.client;
+
+import dev.filipegomes.springboot2.domain.Product;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+@Log4j2
+public class SpringClient {
+    public static void main(String[] args) {
+        ResponseEntity<Product> entity = new RestTemplate().getForEntity("http://localhost:8080/products/{id}", Product.class, 3);
+        log.info(entity);
+
+        Product object = new RestTemplate().getForObject("http://localhost:8080/products/{id}", Product.class, 3);
+
+        log.info(object);
+    }
+}
