02f139d414cb43059a7693c4dcde1c69e9c5da85
filipe gomes <fgdl.py91@gmail.com>
2021-08-12 11:17:24 -0300

    28 - RestTemplate POST

--- src/main/java/dev/filipegomes/springboot2/client/SpringClient.java
+++ src/main/java/dev/filipegomes/springboot2/client/SpringClient.java
@@ -3,8 +3,7 @@ package dev.filipegomes.springboot2.client;
 import dev.filipegomes.springboot2.domain.Product;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.core.ParameterizedTypeReference;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
@@ -25,8 +24,26 @@ public class SpringClient {
         log.info(Arrays.toString(products));
 
         ResponseEntity<List<Product>> exchange =  new RestTemplate().exchange("http://localhost:8080/products/all", HttpMethod.GET, null,
-                new ParameterizedTypeReference<List<Product>>() {});
+                new ParameterizedTypeReference<>() {});
 
         log.info(exchange.getBody());
+
+        // Product car = Product.builder().name("Volvo S90").build();
+        // Product carSaved =  new RestTemplate().postForObject("http://localhost:8080/products/", car, Product.class);
+        // log.info("saved car {}", carSaved);
+
+        Product bike = Product.builder().name("Mountain bike Sutton").build();
+        ResponseEntity<Product> bikeSaved = new RestTemplate().exchange("http://localhost:8080/products/",
+                HttpMethod.POST,
+                new HttpEntity<>(bike, createJsonHeader()),
+                Product.class);
+
+        log.info("saved product {} ", bikeSaved);
+    }
+
+    private static HttpHeaders createJsonHeader() {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        return httpHeaders;
     }
 }
